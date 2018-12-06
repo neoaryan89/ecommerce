4b2d7e2454b57ef1330794c38615bfd8bf16f435
neoaryan89 <neoaryan89@gmail.com>
2018-12-06 07:45:49 -0500

    ecommerce

---
+++ main.py
@@ -0,0 +1,3 @@
+import model.product
+headphones = model.product.Product("simple","headphones")
+print(headphones.product_type)
---
+++ model/product.py
@@ -0,0 +1,10 @@
+class Product:
+
+    product_type = "simple"
+    product_name = ""
+
+    def __init__(self,product_type,product_name):
+        self.product_type = product_type
+        self.product_name = product_name
+
+
